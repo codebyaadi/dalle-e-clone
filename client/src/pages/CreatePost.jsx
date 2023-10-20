99b8ac0e580e08a1baf37b2c0afa3820d9de5640
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2023-10-21 00:16:42 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dall-e-clone-server-59ds.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://dall-e-clone-7ahk.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
