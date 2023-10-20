21ddf813cfa64d90d3d74027b579622fe0348a92
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2023-10-21 00:17:07 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-clone-server-59ds.onrender.com/api/v1/post', {
+      const response = await fetch('https://dall-e-clone-7ahk.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
